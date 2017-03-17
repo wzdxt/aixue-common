c7085d54cc2a59f4dabb60f6b9812dfad205267a
戴鑫通 <xintong.dai@wenba100.com>
2017-03-17 15:42:38 +0800

    1.3.6

---
+++ src/ServiceProviders/CommonServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xintong.dai
+ * Date: 17/3/17
+ * Time: 下午3:35
+ */
+
+namespace Aixue\Common\ServiceProviders;
+
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class CommonServiceProvider extends BaseServiceProvider
+{
+    public function boot()
+    {
+        if (config('app.debug'))
+        {
+            \DB::listen(function($event)
+            {
+                \Log::debug('SQL: ' . $event->sql, [
+                    'bindings' => $event->bindings,
+                    'connection' => $event->connectionName,
+                    'time' => $event->time,
+                ]);
+            });
+        }
+    }
+
+}
