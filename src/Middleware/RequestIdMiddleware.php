f548d58a3da82c9901aa03d7913e63f971636ca3
戴鑫通 <xintong.dai@wenba100.com>
2017-03-22 11:39:44 +0800

    1.3.9

--- src/Middleware/RequestIdMiddleware.php
+++ src/Middleware/RequestIdMiddleware.php
@@ -10,9 +10,15 @@ namespace Aixue\Common\Middleware;
 
 
 use Closure;
+use Illuminate\Http\Request;
 
 class RequestIdMiddleware
 {
+    /**
+     * @param Request $request
+     * @param Closure $next
+     * @return mixed
+     */
     public function handle($request, Closure $next)
     {
         if (empty($_SERVER['HTTP_X_REQUEST_UUID']))
@@ -20,6 +26,7 @@ class RequestIdMiddleware
             $_SERVER['HTTP_X_REQUEST_UUID'] = uniqid();
         }
         app('config')->set('request.request_id', $_SERVER['HTTP_X_REQUEST_UUID']);
+        return $next($request);
     }
 
 }
